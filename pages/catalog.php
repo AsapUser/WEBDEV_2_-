f09422bb3e8caeee8b583c76edaa438186b2dd30
AsapUser <denis133713372@gmail.com>
2020-12-24 01:03:31 +0800

    Added labs

--- pages/catalog.php
+++ pages/catalog.php
@@ -4,6 +4,22 @@
 <link rel="stylesheet" href="style.css" type="text/css"/>
   <head>
     <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <style>
+  #sortable { list-style-type: none; margin: 0; padding: 0; width: 60%; }
+  #sortable li { margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 1.4em; }
+  #sortable li span { position: absolute; margin-left: -1.3em; }
+  </style>
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+  } );
+  </script>
     <title>Каталог</title>
   </head>
   <body>
@@ -16,10 +32,10 @@
   </form>
   <nav class="my-2 my-md-0 mr-md-3">
     <a class="p-2 text-dark" href="about.html">Про нас</a>
-    <a class="p-2 text-dark" href="mainpage.html">Основна сторінка</a>
-    <a class="p-2 text-dark" href="catalog.html">Каталог</a>
-    <a class="p-2 text-dark" href="#">Служба підтримки</a>
-    <a class="p-2 text-dark" href="log.html">Особистий кабінет</a>
+    <a class="p-2 text-dark" href="mainpage.php">Основна сторінка</a>
+    <a class="p-2 text-dark" href="catalog.php">Каталог</a>
+    <a class="p-2 text-dark" href="support.php">Служба підтримки</a>
+    <a class="p-2 text-dark" href="login.php">Особистий кабінет</a>
   </nav>
 </div>
 <script>
@@ -65,15 +81,21 @@ var words = ["Здібні булки","Круасани","Слойки","Тіс
 autocomplete(document.getElementById("myInput"), words);
 </script>
 <h1 class="head">Список продукції</h1>
-<ul>
-  <li>Здобні булки</li>
-  <li>Круасани</li>
-  <li>Слойки</li>
-  <li>Тістечка</li>
-  <li>Торти</li>
-  <li>Печиво</li>
-  <li><b>Хліб</b><i>(звичайний та бездріжжджовий)</i></li>
+<ul id="sortable">
+  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Здобні булки</li>
+  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Круасани</li>
+  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Слойки</li>
+  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Тістечка</li>
+  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Торти</li>
+  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>Печиво</li>
+  <li class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><b>Хліб</b><i>(звичайний та бездріжжджовий)</i></li>
 </ul>
+<form name="bd" method="POST" action="bd.php">
+  <label>Назва товару: <input type="text" name="itname"></label>
+  <input type="submit" name="select" value="Обрати">
+</form>
+<br>
+<br>
 <div class="imager">
 <img src="https://бизнесидея.рф/wp-content/uploads/2018/01/biznes-plan-dlya-mini-pekarni.jpg" id="breadpic">
 <img src="https://moybiznes.org/wp-content/uploads/bulochnaya-678x381.jpg" id="pikcha2">
